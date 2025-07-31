19c7e1e24b6edeffdd8e408f019ecf222bf15095
kevinzekee <kzekeramos@gmail.com>
2025-07-31 13:38:14 +0800

    Improve UI

--- src/pages/FetchPage1.js
+++ src/pages/FetchPage1.js
@@ -2,7 +2,19 @@ import HomeButton from "../components/HomeButton";
 import ShowCode from "../components/ShowCode";
 import '../styles/landing.css';
 
+import React, {use, useEffect, useState} from 'react';
+
+
+
+
+
+
+
+
 function FetchPage1(){
+    const [users, setUsers] = useState([]);
+    const [loading, Loading] = useState(false);
+    const [error, setError] = useState(false);
     return(
         <div className="main-content">
 
