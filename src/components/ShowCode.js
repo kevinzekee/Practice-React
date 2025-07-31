1e35196bd208fdf9294b4114c5020a0dd1dfb8be
kevinzekee <kzekeramos@gmail.com>
2025-07-31 13:49:50 +0800

    Improve UI

--- src/components/ShowCode.js
+++ src/components/ShowCode.js
@@ -1,10 +1,13 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
 function ShowCode(props) {
   return (
     <div style={{ maxWidth: '100%', overflowX: 'auto' }}>
       <SyntaxHighlighter
         language='javascript'
         style={oneDark}
-        wrapLongLines={true}
+        wrapLongLines={true} // this helps with mobile
         customStyle={{
           borderRadius: '6px',
           fontSize: '0.9rem',
@@ -16,3 +19,5 @@ function ShowCode(props) {
     </div>
   );
 }
+
+export default ShowCode;
