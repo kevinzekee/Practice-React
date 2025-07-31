72ce880a70031a2c08726af81a3e8dea48776841
kevinzekee <kzekeramos@gmail.com>
2025-07-31 13:48:33 +0800

    Improve UI

--- src/components/ShowCode.js
+++ src/components/ShowCode.js
@@ -1,12 +1,18 @@
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-
-function ShowCode(props){    
-    return(
-    <SyntaxHighlighter language='javascript' style={oneDark}>
+function ShowCode(props) {
+  return (
+    <div style={{ maxWidth: '100%', overflowX: 'auto' }}>
+      <SyntaxHighlighter
+        language='javascript'
+        style={oneDark}
+        wrapLongLines={true}
+        customStyle={{
+          borderRadius: '6px',
+          fontSize: '0.9rem',
+          padding: '16px',
+        }}
+      >
         {props.codeString}
-    </SyntaxHighlighter>
-    )
+      </SyntaxHighlighter>
+    </div>
+  );
 }
-
-export default ShowCode
